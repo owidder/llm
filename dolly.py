514fcb8f08dafc938b03caf30f7d962ee7f85c89
Oliver Widder <oliver.widder@iteratec.com>
2023-05-09 18:26:48 +0200

    dolly.py, requirements.txt

---
+++ dolly.py
@@ -0,0 +1,44 @@
+import torch
+from langchain.llms.base import LLM
+from llama_index import SimpleDirectoryReader, LangchainEmbedding, GPTListIndex, PromptHelper
+from llama_index import LLMPredictor, ServiceContext
+from transformers import pipeline
+from typing import Optional, List, Mapping, Any
+import os
+
+# define prompt helper
+# set maximum input size
+max_input_size = 2048
+# set number of output tokens
+num_output = 256
+# set maximum chunk overlap
+max_chunk_overlap = 20
+prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+
+class CustomLLM(LLM):
+    model_name = "databricks/dolly-v2-3b"
+    #model_name = "s-JoL/Open-Llama-V1"
+    pipeline = pipeline("text-generation", model=model_name, device="cuda:0", model_kwargs={"torch_dtype":torch.bfloat16}, trust_remote_code=True)
+
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        prompt_length = len(prompt)
+        response = self.pipeline(prompt, max_new_tokens=num_output)[0]["generated_text"]
+        return response
+
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        return {"name_of_model": self.model_name}
+
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+
+
+# define our LLM
+llm_predictor = LLMPredictor(llm=CustomLLM())
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+documents = SimpleDirectoryReader('./data').load_data()
+index = GPTListIndex.from_documents(documents, service_context=service_context)
+query_engine = index.as_query_engine()
+response = query_engine.query("What is a knotterbex policy?")
+print(response)
